c33daf9a7d80c87c4b205687c38a820c4764d101
Nick Chapman <nic@amadido.co.uk>
2025-10-13 08:00:07 +0100

    split out skip_left/right

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ $ time ,jenga/main
 
 Baseline time: 20.35s
 
+Final #steps:10712557522
+
 Plan: Explore use of `comptime` for partial evaluation.
--- main.zig
+++ main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const debug = std.debug;
 const print = debug.print;
 const prog = @embedFile("mandelbrot.b");
+
 pub fn main() !void {
     var mem = [_]u8{0} ** 30_000;
     var ip: usize = 0;
@@ -11,41 +12,46 @@ pub fn main() !void {
         step += 1;
         const op: u8 = prog[ip];
         //print("step:{d}, ip={d}, mp={d}, op='{c}'\n",.{step,ip,mp,op});
-        ip += 1;
         switch(op) {
+            ',' => @panic("input"),
             '.' => print("{c}",.{mem[mp]}),
             '+' => mem[mp] +%= 1,
             '-' => mem[mp] -%= 1,
             '>' => mp += 1,
             '<' => mp -= 1,
-            '[' => {
-                if (mem[mp] == 0) {
-                    var c = prog[ip];
-                    var nest : usize = 0;
-                    while (nest>0 or c != ']') {
-                        if (c == '[') nest += 1;
-                        if (c == ']') nest -= 1;
-                        ip += 1;
-                        c = prog[ip];
-                    }
-                    ip += 1;
-                }
-            },
-            ']' => {
-                if (mem[mp] != 0) {
-                    ip -= 2;
-                    var c = prog[ip];
-                    var nest : usize = 0;
-                    while (nest>0 or c != '[') {
-                        if (c == ']') nest += 1;
-                        if (c == '[') nest -= 1;
-                        ip -= 1;
-                        c = prog[ip];
-                    }
-                    ip += 1;
-                }
-            },
+            '[' => if (mem[mp] == 0) { ip = skip_right(ip+1); },
+            ']' => if (mem[mp] != 0) { ip = skip_left(ip-1); },
             else => {},
         }
+        ip += 1;
     }
+    print("final #steps:{d}\n",.{step});
+}
+
+fn skip_right(ip0: usize) usize {
+    var ip = ip0;
+    var nest : usize = 0;
+    var c = prog[ip];
+    while (nest>0 or c != ']') {
+        if (ip > prog.len) break;
+        if (c == '[') nest += 1;
+        if (c == ']') nest -= 1;
+        ip += 1;
+        c = prog[ip];
+    }
+    return ip;
+}
+
+fn skip_left(ip0: usize) usize {
+    var ip = ip0;
+    var nest : usize = 0;
+    var c = prog[ip];
+    while (nest>0 or c != '[') {
+        if (ip > prog.len) break;
+        if (c == ']') nest += 1;
+        if (c == '[') nest -= 1;
+        ip -= 1;
+        c = prog[ip];
+    }
+    return ip;
 }
